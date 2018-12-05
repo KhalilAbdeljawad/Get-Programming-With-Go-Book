efa22766833a50753711e331e65d072bdf8143c6
Khalil Abdeljawad <khalil@kdevsoft.com>
2018-12-05 21:46:59 +0200

    Update ticketToMars.go

--- ticketToMars.go
+++ ticketToMars.go
@@ -8,25 +8,31 @@ import (
 func main() {
 
 	fmt.Printf("%-18v %-5v %10v %6v\n", "Spaceline", "Days", "Trip type", "Price")
-	fmt.Printf("=====================================\n");
+	fmt.Printf("=====================================\n")
 
-	for count:=10; count>=1; count--{
+	for count := 10; count >= 1; count-- {
 
-		speed:= rand.Intn(16)+15
-		duration := 62100000/speed/(3600*24)
-		price:=36;
+		speed := rand.Intn(16) + 15
+		duration := 62100000 / speed / (3600 * 24)
+		price := 36
 		tripType := "One-Way"
 
-		spacelineName:=""
-		spaceline:= rand.Intn(3)+1
-		switch spaceline{
+		spacelineName := ""
+		spaceline := rand.Intn(3) + 1
+		switch spaceline {
 
-		case 1: spacelineName=("SpaceX ")
-		case 2: spacelineName=("Virgin Galactic ")
-		case 3: spacelineName=("Space Adventure ")
+		case 1:
+			spacelineName = ("SpaceX ")
+		case 2:
+			spacelineName = ("Virgin Galactic ")
+		case 3:
+			spacelineName = ("Space Adventure ")
 		}
-		price+=(62100000/16/(3600*24)-duration-7)
-		if(rand.Intn(2)+1==2){tripType="Round-trip"; price*=2}
+		price += (62100000/16/(3600*24) - duration - 7)
+		if rand.Intn(2)+1 == 2 {
+			tripType = "Round-trip"
+			price *= 2
+		}
 
 		fmt.Printf("%-18v %-5v %10v %6v\n", spacelineName, duration, tripType, price)
 
